914dad731b3a8722f93a99b089610a6411b88595
Bria Trotter <trotter.bria@gmail.com>
2024-06-28 09:25:36 -0400

    Created the Models project

---
+++ ContactWebModels/State.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactWebModels
+{
+    public class State
+    {
+        // Indicates the the Id will be the primary key
+        [Key]
+        public int Id { get; set; }
+
+        // Will map this to a UI field called State
+        [Display(Name = "State")]
+        // Ensures that name is required and provides and error message
+        [Required(ErrorMessage = "Name of State is required")]
+        // Without setting string length, it will be the max VARCHAR length of 4000 characters
+        [StringLength(ContactManagerConstants.MAX_STATE_NAME_LENGTH)] 
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "State Abbreviation is required")]
+        [StringLength(ContactManagerConstants.MAX_STATE_ABBR_LENGTH)]
+        public string Abbreviation { get; set; }
+    }
+}
